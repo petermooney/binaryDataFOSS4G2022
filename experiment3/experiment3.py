cbd93154371c15998cd4af3b229a0c6c5e07ddcd
petermooney <peter.mooney@nuim.ie>
2022-06-03 08:45:34 +0100

    Setting up structure for experiment3 source code.

--- experiment3/experiment3.py
+++ experiment3/experiment3.py
@@ -15,12 +15,12 @@ import numpy as np
 
 getcontext().prec = 16
 file_name = "experiment3"
-#avro_schema_file_name = "address"
-INPUT_GPKG_FILE = '/home/peter/Downloads/Firenze/Firenze_land-use_CLC/Firenze_CLC.gpkg'
+avro_schema_file_name = "clc"
+INPUT_GPKG_FILE = './input-data/Firenze_land-use_CLC_WGS_84_SELECTED.gpkg'
 #INPUT_GPKG_FILE = 'test-geopackage.gpkg'
 #INPUT_GPKG_FILE_LAYER = 'FI_Addresses_Sample_Aug23rd2021'
 
-INPUT_GPKG_FILE_LAYER = 'Firenze_CLC'
+INPUT_GPKG_FILE_LAYER = 'Firenze_land-use_CLC_WGS_84_SELECTED'
 
 geopkg_geojson_timing = []
 geojson_pbf_timing = []
@@ -32,7 +32,7 @@ load_geojson_timing = []
 for test in range (0,1):
 
     ## Read the GeoPackage using GeoPandas and convert to GeoJSON file.
-    """
+
     tic = time.perf_counter()
     print ("===========GeoPackage to GeoJSON==============")
     print ("Begin: Converting GeoPackage to GeoJSON...")
@@ -41,18 +41,18 @@ for test in range (0,1):
     print("GeoPackge File size is {} Kb".format(round(file_size/1024),2))
 
     finland_gdf = gpd.read_file(INPUT_GPKG_FILE, layer=INPUT_GPKG_FILE_LAYER)
-    finland_gdf.to_file("/home/peter/Downloads/Firenze/Firenze_land-use_CLC/geojson-output/{}.geojson".format(file_name), driver='GeoJSON')
+    finland_gdf.to_file("./geojson-output/{}.geojson".format(file_name), driver='GeoJSON')
     print ("End: Converting GeoPackage to GeoJSON...")
     toc = time.perf_counter()
 
     geopkg_geojson_timing.append(toc - tic)
     print(f"Timing: Converting GeoPackage to GeoJSON : {toc - tic:0.4f} seconds")
-    file_size = os.path.getsize("/home/peter/Downloads/Firenze/Firenze_land-use_CLC/geojson-output/{}.geojson".format(file_name))
+    file_size = os.path.getsize("./geojson-output/{}.geojson".format(file_name))
     print("GeoJSON File size is {} Kb".format(round(file_size/1024),2))
-    """
+
     tic = time.perf_counter()
     print ("\nBegin: Loading GeoJSON file for processing...")
-    geojson_data = gpd.read_file("/home/peter/Downloads/Firenze/Firenze_land-use_CLC/geojson-output/{}.geojson".format(file_name))
+    geojson_data = gpd.read_file("./geojson-output/{}.geojson".format(file_name))
     ## obtain the CRS of the data from GeoPandas.
     ## This is important if the CRS is not WGS 84 (EPSG:4326).
     ## Without the CRS specified, a GIS such as QGIS cannot render the GeoJSON file correctly.
@@ -68,6 +68,54 @@ for test in range (0,1):
     print ("GeoJSON file: Dataset Properties: {}, List: {}".format(len(list(geojson_data)),list(geojson_data)))
     print ("End: Loading GeoJSON file for processing...")
 
+    ## fast avro
+    print ("===========GeoJSON to Avro ==============")
+    print ("Serialize GeoJSON to Apache Avro")
+    fast_avro_clc_schema = load_schema("{}.avsc".format(avro_schema_file_name))
+
+    tic = time.perf_counter()
+
+
+    clc_fast_avro = []
+
+    for index, row in geojson_data.iterrows():
+        #fid =  row["fid"]
+        addrHousenumber = row["addr:housenumber"]
+        addrStreet = row["addr:street"]
+        addrCity = row["addr:city"]
+        source = row["source"]
+        addrUnit = row["addr:unit"]
+        fullAddress = row["fullAddress"]
+        geometry = row["geometry"]
+
+        tempCLCAvro = {}
+        tempCLCAvro["addrHousenumber"] = addrHousenumber
+        tempCLCAvro["addrStreet"] = addrStreet
+        tempCLCAvro["addrCity"] = addrCity
+        tempCLCAvro["source"] = source
+        tempCLCAvro["addrUnit"] = addrUnit
+        tempCLCsAvro["fullAddress"] = fullAddress
+
+
+        tempCLCAvro["geometry"] = geometry.to_wkt()
+        tempCLCAvro["fid"] = 1
+
+        clc_fast_avro.append(tempCLCAvro)
+
+
+    with open("./binary-output/{}_fast.avro".format(file_name), "wb") as out:
+        writer(out,fast_avro_address_schema,addresses_fast_avro)
+
+
+    print ("Finished serializing JSON to GeoJSON")
+    toc = time.perf_counter()
+    geojson_avro_timing.append(toc - tic)
+    print(f"Timing Information: Total:\n\tSerialization Process \n\tTotal: {toc - tic:0.4f} seconds")
+    file_size = os.path.getsize("./binary-output/{}_fast.avro".format(file_name))
+    print("File size is {} Kb".format(round(file_size/1024),2))
+
+
+
 
 print ("\n\n\n==== Statistical Report ====")
 pbf_geojson_timing_np = np.array(pbf_geojson_timing)
@@ -82,7 +130,7 @@ print ("=====File Sizes=====")
 file_size = os.path.getsize(INPUT_GPKG_FILE)
 print("Input GPKG file size is {} Kb".format(round(file_size/1024),2))
 
-file_size = os.path.getsize('/home/peter/Downloads/Firenze/Firenze_land-use_CLC/geojson-output/{}.geojson'.format(file_name))
+file_size = os.path.getsize('./geojson-output/{}.geojson'.format(file_name))
 print("./geojson-output/{}.geojson size is {} Kb".format(file_name,round(file_size/1024),2))
 
 
@@ -90,5 +138,3 @@ print ("=====Run Times=====")
 print("Convert GPKG -> GeoJSON mean {:0.3f}s, std-dev {:0.4f}s".format(np.mean(geopkg_geojson_timing_np, dtype=np.float64),np.std(geopkg_geojson_timing_np, dtype=np.float64)))
 
 print("Load GeoJSON mean {:0.3f}s, std-dev {:0.4f}s".format(np.mean(load_geojson_timing_np, dtype=np.float64),np.std(load_geojson_timing_np, dtype=np.float64)))
-
-
